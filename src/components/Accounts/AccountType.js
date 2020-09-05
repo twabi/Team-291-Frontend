4dc5ba1920738188ef313466af6b87afe7a22134
itwabi <itwabi@gmail.com>
2020-09-05 22:10:28 +0200

    changed layout of signIn pages and some images

--- src/components/Accounts/AccountType.js
+++ src/components/Accounts/AccountType.js
@@ -1,50 +1,79 @@
 import React from "react";
-import { 
-    MDBCol, 
-    MDBRow, 
-    MDBCardText, 
+import {
+    MDBCol,
+    MDBRow,
+    MDBCardText,
     MDBIcon,
-    MDBBtn, 
-    MDBCard, 
-    MDBCardBody, 
-    MDBBox, 
-    MDBCardTitle
-    } from "mdbreact";
+    MDBBtn,
+    MDBCard,
+    MDBCardBody,
+    MDBBox,
+    MDBCardTitle, MDBCardFooter
+} from "mdbreact";
+import car from "./mycar.svg";
+import mechanic from "./mechanic.jpg"
 
-const AccountType = ({match}) => 
-(
-    <div> 
-        <h3 className="text-primary mt-5 text-center">Choose Account Type</h3>
-                <MDBBox display="flex" justifyContent="center" alignItems="center" >
-                    <MDBRow className="mt-5">
-                        <MDBCol md="6" className="my-3">
-                            <MDBCard className="d-flex justify-content-center align-items-center"  style={{ width: "22rem" }}>
+const AccountType = (props) => {
 
-                                <MDBIcon far icon="id-card" className="mt-3 indigo-text" size="8x"/>
-                                <MDBCardBody>
-                                    <MDBCardTitle>Driver</MDBCardTitle>
-                                    <MDBCardText>
-                                        Create an account as a driver
-                                    </MDBCardText>
-                                    <MDBBtn href="register/driver" color={"primary"}>GO</MDBBtn>
-                                </MDBCardBody>
-                            </MDBCard>
-                        </MDBCol>
-                        <MDBCol md="6" className="my-3">
-                            <MDBCard className="d-flex justify-content-center align-items-center " style={{ width: "22rem" }}>
-                                <MDBIcon icon="tools" className="mt-3 indigo-text" size="8x"/>
-                                <MDBCardBody>
-                                    <MDBCardTitle>Mechanic</MDBCardTitle>
-                                    <MDBCardText>
-                                        Create an account as a mechanic
-                                    </MDBCardText>
-                                    <MDBBtn href="register/mechanic" color={"primary"}>GO</MDBBtn>
-                                </MDBCardBody>
-                            </MDBCard>
-                        </MDBCol>
-                    </MDBRow>
-                </MDBBox>
-            </div>
-);
+    const handleChoice = (choice) => {
+        props.typeCallback(choice);
+    };
+
+    const handleAccountExists = () => {
+        props.accountCallback();
+    };
+
+    return (
+        <div>
+
+            <MDBBox display="flex" justifyContent="center" alignItems="center" >
+
+                    <MDBCard className="w-100 my-3">
+                        <MDBCardBody>
+                            <h4 className="text-primary mt-5 text-center font-weight-bold">Choose Account Type</h4>
+                            <hr/>
+                            <MDBCardTitle>
+                                <p className="h5 text-primary font-italic text-center font-italic py-2">choose your preferred account type</p>
+                            </MDBCardTitle>
+                            <MDBRow className="mt-2">
+                                <MDBCol md="6" className="my-3">
+                                    <MDBCard style={{height:"20rem"}} className="d-flex justify-content-center text-center align-items-center"  style={{ width: "16rem" }}>
+
+                                        <img style={{width:"15rem", height:"15rem"}} src={car} className="rounded my-2" alt="aligment" />
+                                        <MDBCardBody>
+                                            <MDBCardTitle>Driver</MDBCardTitle>
+                                            <MDBCardText>
+                                                Create an account as a driver
+                                            </MDBCardText>
+                                            <MDBBtn onClick={()=>{handleChoice("Driver")}} color={"primary"}>GO</MDBBtn>
+                                        </MDBCardBody>
+                                    </MDBCard>
+                                </MDBCol>
+                                <MDBCol md="6" className="my-3">
+                                    <MDBCard style={{height:"20rem"}} className="d-flex justify-content-center text-center align-items-center " style={{ width: "16rem" }}>
+                                        <img style={{width:"15rem", height:"10rem"}} src={mechanic} className="rounded my-5" alt="aligment" />
+                                        <MDBCardBody>
+                                            <MDBCardTitle>Mechanic</MDBCardTitle>
+                                            <MDBCardText>
+                                                Create an account as a mechanic
+                                            </MDBCardText>
+                                            <MDBBtn onClick={()=>{handleChoice("Mechanic")}} color={"primary"}>GO</MDBBtn>
+                                        </MDBCardBody>
+                                    </MDBCard>
+                                </MDBCol>
+                            </MDBRow>
+                        </MDBCardBody>
+                        <MDBCardFooter className="justify-content-center font-italic text-center text-info">
+                            <div>
+                                <p onClick={handleAccountExists}>I already have an account</p>
+                            </div>
+                        </MDBCardFooter>
+                    </MDBCard>
+
+            </MDBBox>
+        </div>
+    );
+
+}
 
 export default AccountType;
--- src/components/Accounts/SignIn.js
+++ src/components/Accounts/SignIn.js
@@ -14,6 +14,9 @@ import {
 import "mdbreact/dist/css/mdb.css";
 import {Link} from "react-router-dom";
 import styled from "styled-components";
+import AccountType from "./AccountType";
+import SignUp from "./SignUp";
+import MechRegister from "./Mechregister";
 
 const Background = styled.div`
     background-image: url(${(props) => props.imgUrl});
@@ -28,6 +31,10 @@ const SignInForm = (props) => {
     const [password, setPassword] = React.useState();
     const [showError, setShowError] = React.useState(false);
     const [errorMessage, setErrorMessage] = React.useState();
+    const [showLogin, setShowLogin] = React.useState(true);
+    const [showSignUp, setShowSignUp] = React.useState(false);
+    const [showType, setShowType] = React.useState(false);
+    const [showMechSign, setShowMechSign] = React.useState(false);
 
     const handleNameChange = ({target : {value}}) => {
         setUserName(value);
@@ -36,6 +43,28 @@ const SignInForm = (props) => {
     const handlePassChange = ({target : {value}}) => {
         setPassword(value);
     };
+
+    const typeCallback = (choice) => {
+        if(choice === "Driver"){
+            setShowSignUp(true);
+            setShowLogin(false);
+            setShowMechSign(false);
+            setShowType(false);
+
+        } else if(choice === "Mechanic"){
+            setShowSignUp(false);
+            setShowLogin(false);
+            setShowMechSign(true);
+            setShowType(false);
+        }
+    };
+
+    const handleNoAccount = () => {
+        setShowType(true);
+        setShowSignUp(false);
+        setShowLogin(false);
+        setShowMechSign(false);
+    }
 
     const login = () => {
         if(userName === "" || password === ""){
@@ -47,112 +76,100 @@ const SignInForm = (props) => {
         }
     };
 
+    const alreadyHaveAccount = () =>{
+        setShowType(false);
+        setShowSignUp(false);
+        setShowLogin(true);
+        setShowMechSign(false);
+    }
+
 
     return (
-        <Background imgUrl={process.env.PUBLIC_URL + "/mech.jpg"}>
 
-            <MDBBox display="flex" justifyContent="center" >
-                <MDBCol className="my-4" md="5">
-                    <MDBCard >
-                        <MDBCardBody>
-                            <MDBCardTitle><p className="h4 text-primary text-center py-4">Login</p></MDBCardTitle>
-                            {showError ? <p className="red-text my-4 text-center">{errorMessage}</p> : null}
-                            <form>
-                                <div className="grey-text mx-2">
-                                    <MDBInput
-                                        label="username or email"
-                                        icon="user"
-                                        group
-                                        value={userName}
-                                        onChange={(e) => {handleNameChange(e);}}
-                                        type="text"
-                                        validate
-                                        error="wrong"
-                                        success="right"
-                                    />
-
-
-                                    <MDBInput
-                                        label="enter your password"
-                                        icon="key"
-                                        group
-                                        value={password}
-                                        onChange={(e) => {handlePassChange(e);}}
-                                        type="password"
-                                        validate
-                                        error="wrong"
-                                        success="right"
-                                    />
+            <MDBBox display="flex" justifyContent="center" className="background">
+                    {showLogin ?
+                        <MDBCol className="my-4 mt-2" md="5">
+                            <MDBCard className="mt-2">
+                                <MDBCardBody>
+                                    <h4 className="text-center font-weight-bold text-primary">QuickMechanic App</h4>
+                                    <hr/>
+                                    <MDBCardTitle><p className="h5 text-primary  font-italic text-center py-4">Login</p></MDBCardTitle>
+                                    {showError ? <p className="red-text my-4 text-center">{errorMessage}</p> : null}
+                                    <form className={"mt-5"}>
+                                        <div className="grey-text mx-4 p-4">
+                                            <MDBInput
+                                                label="username or email"
+                                                icon="user"
+                                                group
+                                                value={userName}
+                                                onChange={(e) => {handleNameChange(e);}}
+                                                type="text"
+                                                validate
+                                                outline
+                                                error="wrong"
+                                                success="right"
+                                            />
 
 
-                                </div>
-                                <div className="text-center py-5 my-5">
-                                    <MDBRow className="d-flex flex-row justify-content-center align-content-center">
-                                        <MDBCol md="6" >
-
-                                            <div className="custom-control custom-checkbox">
-                                                <input type="checkbox" className="custom-control-input"
-                                                       id="defaultChecked2"/>
-                                                <label className="custom-control-label text-info" htmlFor="defaultChecked2">
-                                                    Remember Me </label>
-                                            </div>
-
-                                        </MDBCol>
-                                        <MDBCol md="6">
-                                            <MDBBtn onClick={login} color="info">
-                                                login
-                                                <MDBIcon far icon="paper-plane" className="ml-1" />
-                                            </MDBBtn>
-                                        </MDBCol>
-                                    </MDBRow>
+                                            <MDBInput
+                                                label="enter your password"
+                                                icon="key"
+                                                group
+                                                value={password}
+                                                onChange={(e) => {handlePassChange(e);}}
+                                                type="password"
+                                                validate
+                                                outline
+                                                error="wrong"
+                                                success="right"
+                                            />
 
 
-                                </div>
+                                        </div>
+                                        <div className="text-center py-2 my-1">
+                                            <MDBRow className="d-flex flex-row justify-content-center align-content-center">
+                                                <MDBCol md="6" >
 
-                                <div className="row my-3 d-flex justify-content-center">
-                                    <MDBBtn
-                                        type="button"
-                                        color="blue"
-                                        rounded
-                                        className="mr-md-3 w-25 z-depth-1a"
-                                    >
-                                        <MDBIcon fab icon="facebook-f" className="white-text text-center" />
-                                    </MDBBtn>
-                                    <MDBBtn
-                                        type="button"
-                                        color="light-blue"
-                                        rounded
-                                        className="mr-md-3 w-25 z-depth-1a"
-                                    >
-                                        <MDBIcon fab icon="twitter" className="white-text" />
-                                    </MDBBtn>
-                                    <MDBBtn
-                                        type="button"
-                                        color="red"
-                                        rounded
-                                        className="z-depth-1a w-25"
-                                    >
-                                        <MDBIcon fab icon="google-plus-g" className="white-text" />
-                                    </MDBBtn>
-                                </div>
-                            </form>
+                                                    <div className="custom-control custom-checkbox">
+                                                        <input type="checkbox" className="custom-control-input"
+                                                               id="defaultChecked2"/>
+                                                        <label className="custom-control-label text-info" htmlFor="defaultChecked2">
+                                                            Remember Me </label>
+                                                    </div>
+
+                                                </MDBCol>
+                                                <MDBCol md="6">
+                                                    <MDBBtn onClick={login} color="info">
+                                                        login
+                                                    </MDBBtn>
+                                                </MDBCol>
+                                            </MDBRow>
 
 
-                        </MDBCardBody>
-                        <MDBCardFooter className="justify-content-center font-italic text-center text-info">
-                            <div>
-                                <Link to="/register">I don't have an account</Link>
-                            </div>
-                        </MDBCardFooter>
-                    </MDBCard>
+                                        </div>
 
-                </MDBCol>
+                                    </form>
+
+
+                                </MDBCardBody>
+                                <MDBCardFooter className="justify-content-center font-italic text-center text-info">
+                                    <div>
+                                        <p onClick={handleNoAccount}>I don't have an account</p>
+                                    </div>
+                                </MDBCardFooter>
+                            </MDBCard>
+
+                        </MDBCol>
+                        :null}
+
+
+                    {showType? <AccountType accountCallback={alreadyHaveAccount} typeCallback={typeCallback}/>:null}
+                    {showSignUp? <SignUp accountCallback={alreadyHaveAccount}/> : null}
+                    {showMechSign? <MechRegister accountCallback={alreadyHaveAccount}/>: null}
 
 
             </MDBBox>
 
-
-        </Background>
     );
 };
 
