944e1f111c2bbdebc13a55e7da3347d4a0ce20be
itwabi <itwabi@gmail.com>
2020-10-04 10:47:36 +0200

    changes from Sandra

--- src/components/Accounts/Mechregister.js
+++ src/components/Accounts/Mechregister.js
@@ -1,23 +1,14 @@
 import React from "react";
-import {
-    MDBCol,
-    MDBInput,
-    MDBCardFooter,
+import { 
+    MDBCol, 
+    MDBInput, 
     MDBIcon,
     MDBBtn,
-    MDBCard,
-    MDBCardBody,
-    MDBBox,
-    MDBCardTitle
-} from "mdbreact";
-import { Input } from "antd";
+    MDBBox, 
+    } from "mdbreact";
+import {Input} from "antd";
 import "antd/dist/antd.css";
-import { fieldNameFromStoreName } from "@apollo/client/cache/inmemory/helpers";
-
-
-
-
-
+import { gql, useMutation } from "@apollo/client";
 
 
 const MechRegister = (props) => {
@@ -25,7 +16,7 @@ const MechRegister = (props) => {
     const [loading, setLoading] = React.useState(false);
     const [fileState, setFileState] = React.useState({});
 
-    const handleFileUpload = (e) => {
+        const handleFileUpload = (e) => {
 
         const reader = new FileReader();
         const file = e.target.files[0];
@@ -46,8 +37,8 @@ const MechRegister = (props) => {
         var location = document.getElementById("relativeLocation").value;
         var companyName = document.getElementById("companyName").value;
         var phoneNumber = document.getElementById("phoneNumber").value;
-        console.log(fileState);
-        console.log(userName, password, location, companyName, phoneNumber);
+        //console.log(fileState);
+        //console.log(userName, password, location, companyName, phoneNumber);
 
         if (userName.length === 0 || email.length === 0 || password.length === 0 || companyName.length === 0
             || phoneNumber.length === 0 || fileState.length === 0 || location.length === 0) {
@@ -58,8 +49,7 @@ const MechRegister = (props) => {
             navigator.geolocation.getCurrentPosition((position) => {
                 const userCoordinates = [position.coords.longitude, position.coords.latitude];
 
-                console.log("HHH", userCoordinates);
-
+                //console.log("HHH", userCoordinates);
 
                 //instead of ${userCoordinates[0]} a for loop or we must create two variables latitude and longitude and store both data in the varaiblle concerned
 
@@ -79,7 +69,7 @@ const MechRegister = (props) => {
                             }
                             `
                 };
-                fetch("https://secret-citadel-57463.herokuapp.com/graphql", {
+                fetch("https://cors-anywhere.herokuapp.com/https://secret-citadel-57463.herokuapp.com/graphql", {
                     method: "POST",
                     body: JSON.stringify(request),
                     headers: {
@@ -120,116 +110,119 @@ const MechRegister = (props) => {
         props.typeCallback();
     };
 
-    return (
-        <MDBBox display="flex" className="w-100" justifyContent="center" >
-            <MDBCol className="w-100">
-                <div>
-                    <MDBBtn color="primary" onClick={gotoTypes} className="float-left mx-1 mb-3">
-                        <MDBIcon icon="angle-double-left" className="white-text" size="1x" />
-                    </MDBBtn><h5 className="h4 mx-1 text-primary text-center">Mechanic Account</h5>
-                </div>
-                <form className="mt-5 mx-2 grey-text">
-                    <div className="md-form d-flex flex-column justify-content-center align-items-center mt-2 mx-1">
-                        <Input type="file"
-                            style={{ width: "50%" }}
-                            accept="image/*"
-                            onChange={handleFileUpload}
-                            size="large"
-                            placeholder="Select profile Image" prefix={<MDBIcon icon="user-plus" />} />
-                        {/* <p className="font-italic">{fileState.name}</p> */}
+        return (
+            <MDBBox display="flex" className="w-100" justifyContent="center" >
+                <MDBCol className="w-100">
+                    <div>
+                        <MDBBtn color="primary" onClick={gotoTypes} className="float-left mx-1 mb-3">
+                            <MDBIcon icon="angle-double-left" className="white-text" size="1x"/>
+                        </MDBBtn><h5 className="h4 mx-1 text-primary text-center">Mechanic Account</h5>
                     </div>
-                    <MDBInput
-                        label="enter your user name"
-                        id="username"
-                        icon="user"
-                        group
-                        type="text"
-                        validate
-                        outline
-                        error="wrong"
-                        success="right"
-                    />
+                    <form className="mt-5 mx-2 grey-text">
+                        <div className="md-form d-flex flex-column justify-content-center align-items-center mt-2 mx-1">
+                            <Input type="file"
+                                   style={{ width: "50%" }}
+                                   accept="image/*"
+                                   onChange={handleFileUpload}
+                                   size="large"
+                                   placeholder="Select profile Image" prefix={<MDBIcon icon="user-plus"/>} />
+                            {/* <p className="font-italic">{fileState.name}</p> */}
+                        </div>
+                        <MDBInput
+                            label="enter your user name"
+                            id="username"
+                            icon="user"
+                            group
+                            type="text"
+                            validate
+                            outline
+                            error="wrong"
+                            success="right"
+                        />
 
-                    <MDBInput
-                        label="enter your email"
-                        id="email"
-                        icon="envelope"
-                        group
-                        type="email"
-                        validate
-                        outline
-                        error="wrong"
-                        success="right"
-                    />
+                        <MDBInput
+                            label="enter your email"
+                            id="email"
+                            icon="envelope"
+                            group
+                            type="email"
+                            validate
+                            outline
+                            error="wrong"
+                            success="right"
+                        />
 
-                    <MDBInput
-                        label="enter your phone number"
-                        icon="phone-alt"
-                        id="phoneNumber"
-                        group
-                        type="number"
-                        validate
-                        outline
-                        error="wrong"
-                        success="right"
-                    />
+                        <MDBInput
+                            label="enter your phone number"
+                            icon="phone-alt"
+                            id="phoneNumber"
+                            group
+                            type="number"
+                            validate
+                            outline
+                            error="wrong"
+                            success="right"
+                        />
 
-                    <MDBInput
-                        label="enter your garage name"
-                        icon="building"
-                        id="companyName"
-                        group
-                        type="text"
-                        validate
-                        outline
-                        error="wrong"
-                        success="right"
-                    />
+                        <MDBInput
+                            label="enter your garage name"
+                            icon="building"
+                            id="companyName"
+                            group
+                            type="text"
+                            validate
+                            outline
+                            error="wrong"
+                            success="right"
+                        />
 
-                    <MDBInput
-                        label="enter your garage location e.g City, Area"
-                        icon="map-marker-alt"
-                        id="relativeLocation"
-                        group
-                        type="text"
-                        validate
-                        outline
-                        error="wrong"
-                        success="right"
-                    />
+                        <MDBInput
+                            label="enter your garage location e.g City, Area"
+                            icon="map-marker-alt"
+                            id="relativeLocation"
+                            group
+                            type="text"
+                            validate
+                            outline
+                            error="wrong"
+                            success="right"
+                        />
 
-                    <MDBInput
-                        label="enter your preferred password"
-                        icon="key"
-                        id="password"
-                        group
-                        type="password"
-                        validate
-                        outline
-                        error="wrong"
-                        success="right"
-                    />
+                        <MDBInput
+                            label="enter your preferred password"
+                            icon="key"
+                            id="password"
+                            group
+                            type="password"
+                            validate
+                            outline
+                            error="wrong"
+                            success="right"
+                        />
 
-                    <div className="text-center py-4 mt-5">
-                        <MDBBtn color="primary" onClick={handleCreateMechanic} className="text-white">
-                            Register
+                        <div className="text-center py-4 mt-5">
+                            <MDBBtn color="primary" onClick={() => {handleCreateMechanic();}} className="text-white">
+                                Register
                                 {loading ? <div className="spinner-border ml-2 spinner-border-sm" role="status">
-                                <span className="sr-only">Loading...</span>
-                            </div> : null}
-                        </MDBBtn>
+                                    <span className="sr-only">Loading...</span>
+                                </div> : null }
+                            </MDBBtn>
+                        </div>
+                    </form>
+
+                    <hr/>
+                    <div className="text-center d-flex flex-column font-italic text-primary">
+                        <a onClick={handleAccountExists}>I already have an account</a>
+                        <a href="https://assistant.google.com/services/invoke/uid/0000002b95f10945?hl=en">
+                            🅖 Ask my test app to help me out
+                        </a>
                     </div>
-                </form>
-
-                <hr />
-                <div className="text-center font-italic text-primary">
-                    <a onClick={handleAccountExists}>I already have an account</a>
-                </div>
-            </MDBCol>
+                </MDBCol>
 
 
-        </MDBBox>
-    );
-};
+            </MDBBox>
+        );
+    };
 
 
-export default MechRegister;
+    export default MechRegister;
