9624d2b7a352c26f6d0903c28e2abebf39ac4277
itwabi <itwabi@gmail.com>
2020-10-04 10:52:20 +0200

    codacy cleanup

--- src/components/Accounts/Mechregister.js
+++ src/components/Accounts/Mechregister.js
@@ -27,6 +27,11 @@ const MechRegister = (props) => {
         setFileState(file);
     };
 
+    const gotoTypes = () => {
+        props.typeCallback();
+    };
+
+
     const handleCreateMechanic = () => {
 
         setLoading(true);
@@ -69,7 +74,7 @@ const MechRegister = (props) => {
                             }
                             `
                 };
-                fetch("https://cors-anywhere.herokuapp.com/https://secret-citadel-57463.herokuapp.com/graphql", {
+                fetch("https://secret-citadel-57463.herokuapp.com/graphql", {
                     method: "POST",
                     body: JSON.stringify(request),
                     headers: {
@@ -87,7 +92,7 @@ const MechRegister = (props) => {
 
                         return result.json();
                     }).then((resData) => {
-                        console.log(resData)
+                        //console.log(resData)
                     })
                     .catch((error) => {
                         setLoading(false);
@@ -95,134 +100,128 @@ const MechRegister = (props) => {
                     });
 
             });
-
-
         }
-
-
-    }
+    };
 
     const handleAccountExists = () => {
         props.accountCallback();
     };
 
-    const gotoTypes = () => {
-        props.typeCallback();
-    };
 
-        return (
-            <MDBBox display="flex" className="w-100" justifyContent="center" >
-                <MDBCol className="w-100">
-                    <div>
-                        <MDBBtn color="primary" onClick={gotoTypes} className="float-left mx-1 mb-3">
-                            <MDBIcon icon="angle-double-left" className="white-text" size="1x"/>
-                        </MDBBtn><h5 className="h4 mx-1 text-primary text-center">Mechanic Account</h5>
+
+    return (
+        <MDBBox display="flex" className="w-100" justifyContent="center" >
+            <MDBCol className="w-100">
+                <div>
+                    <MDBBtn color="primary" onClick={gotoTypes} className="float-left mx-1 mb-3">
+                        <MDBIcon icon="angle-double-left" className="white-text" size="1x"/>
+                    </MDBBtn><h5 className="h4 mx-1 text-primary text-center">Mechanic Account</h5>
+                </div>
+                <form className="mt-5 mx-2 grey-text">
+                    <div className="md-form d-flex flex-column justify-content-center align-items-center mt-2 mx-1">
+                        <Input type="file"
+                               style={{ width: "50%" }}
+                               accept="image/*"
+                               onChange={handleFileUpload}
+                               size="large"
+                               placeholder="Select profile Image" prefix={<MDBIcon icon="user-plus"/>} />
+                        {/* <p className="font-italic">{fileState.name}</p> */}
                     </div>
-                    <form className="mt-5 mx-2 grey-text">
-                        <div className="md-form d-flex flex-column justify-content-center align-items-center mt-2 mx-1">
-                            <Input type="file"
-                                   style={{ width: "50%" }}
-                                   accept="image/*"
-                                   onChange={handleFileUpload}
-                                   size="large"
-                                   placeholder="Select profile Image" prefix={<MDBIcon icon="user-plus"/>} />
-                            {/* <p className="font-italic">{fileState.name}</p> */}
-                        </div>
-                        <MDBInput
-                            label="enter your user name"
-                            id="username"
-                            icon="user"
-                            group
-                            type="text"
-                            validate
-                            outline
-                            error="wrong"
-                            success="right"
-                        />
+                    <MDBInput
+                        label="enter your user name"
+                        id="username"
+                        icon="user"
+                        group
+                        type="text"
+                        validate
+                        outline
+                        error="wrong"
+                        success="right"
+                    />
 
-                        <MDBInput
-                            label="enter your email"
-                            id="email"
-                            icon="envelope"
-                            group
-                            type="email"
-                            validate
-                            outline
-                            error="wrong"
-                            success="right"
-                        />
+                    <MDBInput
+                        label="enter your email"
+                        id="email"
+                        icon="envelope"
+                        group
+                        type="email"
+                        validate
+                        outline
+                        error="wrong"
+                        success="right"
+                    />
 
-                        <MDBInput
-                            label="enter your phone number"
-                            icon="phone-alt"
-                            id="phoneNumber"
-                            group
-                            type="number"
-                            validate
-                            outline
-                            error="wrong"
-                            success="right"
-                        />
+                    <MDBInput
+                        label="enter your phone number"
+                        icon="phone-alt"
+                        id="phoneNumber"
+                        group
+                        type="number"
+                        validate
+                        outline
+                        error="wrong"
+                        success="right"
+                    />
 
-                        <MDBInput
-                            label="enter your garage name"
-                            icon="building"
-                            id="companyName"
-                            group
-                            type="text"
-                            validate
-                            outline
-                            error="wrong"
-                            success="right"
-                        />
+                    <MDBInput
+                        label="enter your garage name"
+                        icon="building"
+                        id="companyName"
+                        group
+                        type="text"
+                        validate
+                        outline
+                        error="wrong"
+                        success="right"
+                    />
 
-                        <MDBInput
-                            label="enter your garage location e.g City, Area"
-                            icon="map-marker-alt"
-                            id="relativeLocation"
-                            group
-                            type="text"
-                            validate
-                            outline
-                            error="wrong"
-                            success="right"
-                        />
+                    <MDBInput
+                        label="enter your garage location e.g City, Area"
+                        icon="map-marker-alt"
+                        id="relativeLocation"
+                        group
+                        type="text"
+                        validate
+                        outline
+                        error="wrong"
+                        success="right"
+                    />
 
-                        <MDBInput
-                            label="enter your preferred password"
-                            icon="key"
-                            id="password"
-                            group
-                            type="password"
-                            validate
-                            outline
-                            error="wrong"
-                            success="right"
-                        />
+                    <MDBInput
+                        label="enter your preferred password"
+                        icon="key"
+                        id="password"
+                        group
+                        type="password"
+                        validate
+                        outline
+                        error="wrong"
+                        success="right"
+                    />
 
-                        <div className="text-center py-4 mt-5">
-                            <MDBBtn color="primary" onClick={() => {handleCreateMechanic();}} className="text-white">
-                                Register
-                                {loading ? <div className="spinner-border ml-2 spinner-border-sm" role="status">
-                                    <span className="sr-only">Loading...</span>
-                                </div> : null }
-                            </MDBBtn>
-                        </div>
-                    </form>
-
-                    <hr/>
-                    <div className="text-center d-flex flex-column font-italic text-primary">
-                        <a onClick={handleAccountExists}>I already have an account</a>
-                        <a href="https://assistant.google.com/services/invoke/uid/0000002b95f10945?hl=en">
-                            🅖 Ask my test app to help me out
-                        </a>
+                    <div className="text-center py-4 mt-5">
+                        <MDBBtn color="primary" onClick={() => {handleCreateMechanic();}} className="text-white">
+                            Register
+                            {loading ? <div className="spinner-border ml-2 spinner-border-sm" role="status">
+                                <span className="sr-only">Loading...</span>
+                            </div> : null }
+                        </MDBBtn>
                     </div>
-                </MDBCol>
+                </form>
+
+                <hr/>
+                <div className="text-center d-flex flex-column font-italic text-primary">
+                    <a onClick={handleAccountExists}>I already have an account</a>
+                    <a href="https://assistant.google.com/services/invoke/uid/0000002b95f10945?hl=en">
+                        🅖 Ask my test app to help me out
+                    </a>
+                </div>
+            </MDBCol>
 
 
-            </MDBBox>
-        );
-    };
+        </MDBBox>
+    );
+};
 
 
-    export default MechRegister;
+export default MechRegister;
